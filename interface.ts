ee883acdc072b88bb5bd40f2c1013e822640dbcd
여다희 <yeodahui.dev@gmail.com>
2024-01-10 18:12:18 +0900

    interface 사용하기

---
+++ interface.ts
@@ -0,0 +1,41 @@
+enum Size {
+  S = "S",
+  M = "M",
+  L = "L",
+  XL = "XL",
+}
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  salePrice?: number;
+  membersOnly?: boolean;
+}
+
+interface ClothingProduct extends Product {
+  sizes: Size[];
+}
+
+const product1: ClothingProduct = {
+  id: "c001",
+  name: "코드잇 블랙 후드 집업",
+  price: 129000,
+  membersOnly: true,
+  sizes: [Size.M, Size.L],
+};
+
+const product2: ClothingProduct = {
+  id: "c002",
+  name: "코드잇 그레이 후드 집업",
+  price: 129000,
+  membersOnly: true,
+  sizes: [Size.M, Size.L],
+};
+
+function printProduct(product: Product) {
+  console.log(`${product.name}의 가격은 ${product.price}원입니다.`);
+}
+
+printProduct(product1);
+printProduct(product2);
