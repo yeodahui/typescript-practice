f5f3f881f5c4701ef8284ff02429738e551573b2
여다희 <yeodahui.dev@gmail.com>
2024-01-12 10:22:08 +0900

    Union 타입 in 조건 연산을 통한 내로잉

--- union.ts
+++ union.ts
@@ -46,3 +46,45 @@ if (typeof researcher == "string") {
 typeof researcher === "string"
   ? researcher.toUpperCase()
   : researcher.toFixed();
+
+enum ClothingSize {
+  S = "S",
+  M = "M",
+  L = "L",
+  XL = "XL",
+}
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  membersOnly?: boolean;
+}
+
+interface ClothingProduct extends Product {
+  sizes: ClothingSize[];
+  color: string;
+}
+
+type ShoeSize = 220 | 225 | 230 | 235 | 240 | 245 | 250 | 255;
+
+interface ShoeProduct extends Product {
+  sizes: number[];
+  handmade: boolean;
+}
+
+function printSizes(product: ClothingProduct | ShoeProduct) {
+  const availableSizes = product.sizes.join(", ");
+  console.log(`구매 가능한 사이즈는 다음과 같습니다: ${availableSizes}`);
+
+  if ("color" in product) {
+    console.log(`색상: ${product.color}`);
+  }
+  if ("handmade" in product) {
+    console.log(
+      product.handmade === true
+        ? "이 상품은 장인이 직접 만듭니다."
+        : "이 상품은 공장에서 만들어졌습니다."
+    );
+  }
+}
