5cef8e482e8e9881c36358eb7239ad81f5e8471d
여다희 <yeodahui.dev@gmail.com>
2024-01-12 22:32:05 +0900

    interface intersection시 중복 속성이 있어도 ok

--- intersection.ts
+++ intersection.ts
@@ -82,4 +82,36 @@ interface Review extends TimestampEntity {
   content: string;
 }
 
+/* 중복되는 속성이 있어도 병합 가능하다 */
+interface Equipment {
+  id: string;
+  name: string;
+  price: number;
+}
+
+interface Weapon extends Equipment {
+  attack: number;
+}
+
+interface Armor extends Equipment {
+  defence: number;
+}
+
+function printEquipment(equipment: Weapon & Armor) {
+  console.log(`이름: ${equipment.name}`);
+  console.log(
+    `이 장비는 공격력을 ${equipment.attack}, 방어력을 ${equipment.defence} 증가 시킵니다.`
+  );
+}
+
+const item1: Weapon & Armor = {
+  id: "g001",
+  name: "서리불꽃 글러브",
+  price: 100,
+  attack: 5,
+  defence: 42,
+};
+
+printEquipment(item1);
+
 // 두 가지 중 어떤 방법을 사용해야 할까?
