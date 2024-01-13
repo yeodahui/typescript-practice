8e3f24d67927793a7638476bba3ebedb8195ea94
여다희 <yeodahui.dev@gmail.com>
2024-01-13 10:45:00 +0900

    union 타입과 내로잉, 타입검사

--- union.ts
+++ union.ts
@@ -1,23 +1,30 @@
-// 유니온 타입 추론
-let physicist = Math.random() > 0.5 ? "Marie Curie" : 84;
+/**
+ * 유니온 타입
+ * 값이 정확히 어떤 타입인지 모르지만 두 개 이상의 옵션 중 하나라는 것을 알고 있는 경우 사용한다.
+ * |(수직선) 연산자를 사용해 나타낸다.
+ */
 
-// number.toString() OK
-// string.toString() OK
-physicist.toString(); // OK
-
-// number.toUpperCase() X
-// string.toUpperCase() OK
-// physicist.toUpperCase(); // Error
-
-// 내로잉: 유니언 타입 중 더 구체적인 타입으로 좁히는 것
 let admiral: number | string;
 
 admiral = "Grace Hoper";
 
-admiral.toUpperCase(); // OK
-// admiral.toFixed(); // Error
+// 타입 추론:
+// physicist는 유니온 타입으로 추론된다.
+let physicist = Math.random() > 0.5 ? "Marie Curie" : 84;
 
-/* 타입 가드 */
+// 값이 유니언 타입일 때, 유니언으로 선언한 모든 가능한 타입에 존재하는 멤버 속성에만 접근할 수 있다.
+// number 타입과 stirng 타입 모두 .toString()메소드를 가지고 있으므로 다음과 같이 toString 메소드에 접근이 가능하다.
+physicist.toString(); // OK
+
+// string 타입은 toUpperCase() 메소드를 가지고 있으나 number는 가지고 있지 않으므로 다음과 같은 코드는 에러가 발생한다.
+physicist.toUpperCase(); // Error
+
+/**
+ * 내로잉
+ * 유니언 타입으로 정의된 여러 타입 중 하나의 타입으로 된 값의 속성을 사용하려면 코드에서 값이 보다 구체적인 타입(specific type) 중 하나라는 것을 타입스크립트에 알려야 한다.
+ */
+
+/* 타입 가드: 타입을 좁히는 데 사용할 수 있는 논리적 검사 */
 // 1. 값 할당을 통한 내로잉
 let inventor: number | string = "Hedy Lamarr";
 
