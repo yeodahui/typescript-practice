524f9f2434c8473c82cf3f7acf998f724031443a
여다희 <yeodahui.dev@gmail.com>
2024-01-16 23:16:44 +0900

    React Props 타입 정의 설명 추가

--- codeit-react-ts/src/components/Button.tsx
+++ codeit-react-ts/src/components/Button.tsx
@@ -1,3 +1,6 @@
+// Props
+// 인터페이스를 사용해 타입을 지정, children의 경우 ReactNode라는 타입을 사용한다.
+
 import { MouseEvent, ReactNode } from "react";
 import styles from "./Button.module.css";
 
--- codeit-react-ts/src/components/Input.tsx
+++ codeit-react-ts/src/components/Input.tsx
@@ -1,3 +1,6 @@
+// HTML 기본 속성을 Props 타입으로 정의하고 싶다면
+// [태그이름]HTMLAttributes<[노드타입]> 형태의 타입을 상속해 활용할 수 있다.
+
 import { InputHTMLAttributes } from "react";
 import styles from "./Input.module.css";
 
